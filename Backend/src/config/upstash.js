3946c76de83c2f9b5c68fed8f1d824d889e4bdca
Pranit Kolhe <pranitkolhe3@gmail.com>
2025-11-04 13:47:11 +0530

    Increase in time for rate limitter

--- Backend/src/config/upstash.js
+++ Backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config();
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
     // 15 requests in 20 seconds
-    limiter: Ratelimit.slidingWindow(5,"15 s"),
+    limiter: Ratelimit.slidingWindow(8,"15 s"),
 });
 
 export default ratelimit;
